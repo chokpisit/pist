3bf1586c04bda937533c28c3e752d4996a59a69f
chokpisit <sense7999@gmail.com>
2018-09-09 01:28:16 +0700

    work2

--- seq3.py
+++
@@ -1,9 +0,0 @@
-"""seq3"""
-def ses():
-    """num + j +i = !!!!!!"""
-    num = int(input())
-    for i in range(num):
-        for j in range(num):
-            print(2+j+i, end=" ")
-        print("")
-ses()
