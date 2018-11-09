b9f32065abd31f0b30991f9898daa06f372f9589
chokpisit liwrungsub <42942073+chokpisit@users.noreply.github.com>
2018-11-09 12:01:34 +0700

    Delete seq.py

--- seq.py
+++
@@ -1,9 +0,0 @@
-"""Sequence I"""
-def main():
-    """input num"""
-    num = int(input())
-    for _ in range(num):
-        for i in range(num):
-            print(i+1, end=" ")
-        print("")
-main()
